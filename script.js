7ada5148056446ea7594c30b5744b75184ba82a3
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-26 19:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll(".code");
+codes.forEach((code, index) => {
+  code.addEventListener("keydown", (e) => {
+    e.preventDefault();
+    let val = e.key;
+    // code.value = val;
+    if (val == "Backspace") focusPrev(index);
+    else focusNext(index, val);
+  });
+});
+
+function focusNext(idx, val) {
+  codes[idx].value = val;
+  let next = idx + 1;
+  if (next >= 6) return;
+  let c = codes[next];
+  c.focus();
+  c.value = "";
+}
+function focusPrev(idx) {
+  codes[idx].value = "";
+  let pre = idx - 1;
+  if (pre < 0) return;
+  let c = codes[pre];
+  c.focus();
+}
